41e106a8e8553c998fe34d6baee4905627c1d69a
koray <koray@CYGNUS>
2025-03-27 16:07:16 +0300

    Add project files.

---
+++ 6-SansOyunu/Form2.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace _6_SansOyunu
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(127, 85);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(92, 136);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 55);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Üret";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 293);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ 6-SansOyunu/Form2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6_SansOyunu
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] sembol1 = { "a", "b", "c", "d", "e", "f", "g" };
+            string[] sembol2 = { "+", "-", "*", "/" };
+
+            Random r = new Random();
+            int s1, s2, s3;
+            s1 = r.Next(0, sembol1.Length);
+            s2 = r.Next(0, sembol2.Length);
+            s3 = r.Next(1, 10);
+
+            label5.Text = sembol1[s1].ToString() + sembol2[s2].ToString() + s3;
+        }
+
+    }
+}
